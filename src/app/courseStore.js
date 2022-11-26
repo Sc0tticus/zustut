e2bea827ce294bc16191fa32654f6cbe409b2f41
Sc0tticus <ssinger303@gmail.com>
2022-11-26 13:46:16 -0700

    Creating courseStore

---
+++ src/app/courseStore.js
@@ -0,0 +1,35 @@
+import create from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+// set is used to change the state of any variable
+const courseStore = (set) => ({
+	courses: [], // initial state
+	addCourse: (course) => {
+		set((state) => ({
+			courses: [course, ...state.courses]
+		}))
+	},
+	removeCourse: (courseId) => {
+		set((state) => ({
+			courses: state.courses.filter((course) => course.id !== courseId)
+		}))
+	},
+	toggleCourseStatus: (courseId) => {
+		set((state) => ({
+			courses: state.courses.map((course) => course.id === "completed" ? {
+				course, completed: !course.completed
+			} : course)
+		}))
+	}
+})
+
+const useCourseStore = create(
+	devtools(
+		persist(courseStore, {
+			name: "courses"
+		})
+	)
+)
+
+export default useCourseStore;
+
