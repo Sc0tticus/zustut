65076693af9919ca01cbc65ffd971b773d919b8e
Sc0tticus <ssinger303@gmail.com>
2022-11-26 16:46:42 -0700

    Finishing YT Part 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
 		  }}>
 			  MyCourseList
 		  </h1>
-	  <CourseForm />
+		  <CourseForm />
+		  <CourseList />
     </div>
   );
 }
